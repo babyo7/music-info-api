92d39051efb4ff4184f5d01ce1878794d96e6085
Aman <119067199+babyo7@users.noreply.github.com>
2024-02-17 11:05:11 +0530

    Update playlist.js

--- routes/playlist.js
+++ routes/playlist.js
@@ -46,6 +46,7 @@ router.get("/:s?", async (req, res) => {
   } catch (error) {
     if (error.message == "Cannot read properties of null (reading 'split')") {
       res.status(200).json(de);
+      return;
     }
     res.status(500).json({ message: error.message });
   }
